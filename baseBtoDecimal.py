a8a1809ea63b101d91bfb32714994c0b51868e50
Milton Filho <milton.filho@armazemparaiba.com.br>
2017-11-28 13:49:03 -0300

    corrige print do resultado

--- baseBtoDecimal.py
+++ baseBtoDecimal.py
@@ -2,17 +2,17 @@
 
 numero = str(input("Informe o numero: ")) # TRANSFORMA INTEIRO EM STRING (PARA INTERAR ABAIXO)
 base = int(input("Informe a base: ")) # LE A BASE
-numeroDecimal = []
-tamanhoNumero = len(str(numero)) # QUANTIDADE DE DIGITOS DO NUMERO
+numeroDecimal = [] # lista/vetor para acumular os digitos calculados na interacao 1
+tamanhoNumero = len(str(numero)) # QUANTIDADE DE DIGITOS DO NUMERO para calcular o valor exponencial
 
-# FUNCAO RETORNA CADA DIGITO E A POSICAO DELE
+# interacao 1 - FUNCAO RETORNA CADA DIGITO E A POSICAO DELE
 for posicao, digito in enumerate(numero):
     digito = int(digito)
-    numeroDecimal.append(digito * (base ** (tamanhoNumero - posicao)))
+    numeroDecimal.append(digito * (base ** (tamanhoNumero - posicao))) 
 
 # SOMA OS RESULTADOS
 resultado = 0
 for digito in numeroDecimal:
     resultado += digito
 
-print("Base 10 > {}".format(numero, base, resultado))
+print("Base 10 > {}".format(resultado))
